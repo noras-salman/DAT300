ce22733ae95acc8447e99bd4f8c9421b46cf6f10
noras-salman <noras.salman@live.com>
2016-09-27 19:54:58 +0200

    fixed naming and more removing files

--- data_optimize/_data_prepares2.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import matplotlib.pyplot as plt
-from numpy import loadtxt
-
-#Load data with help of numpy
-data = loadtxt('winter2006-2007.txt', delimiter=';')
-row1=0
-col8=7
-print data[row1][col8]
