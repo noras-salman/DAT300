7fe1b473c5dc4f22e29ab10f82b4065c582ae6ba
noras-salman <noras.salman@live.com>
2016-10-03 09:35:14 +0200

    final two datasets completed !

--- README.md
+++ README.md
@@ -12,18 +12,13 @@ The structure is seperated to two directories:
 
 ## DATASETS
 Final datasets constucted from 'winter2006-2007.txt' could be found in the regression_template directory and they are:
-- hour_basic : (weekday,hour,total_load) 
-- hour_weekend : (weekday,hour,weekend_marker,total_load)
+- hour_basic : (weekday,hour,total_load)   "**COMPLETED**"
+- hour_weekend : (weekday,hour,weekend_marker,total_load) : adds extra feature "weekend_marker" is a value that state if its a weekday or weekend  "**COMPLETED**"
+- hour_basic_prev_hour : (weekday,hour,previous_hour,total_load) : adds extra feature "previous_hour" "**COMPLETED**"
+- hour_basic_prev_2hours : (weekday,hour,previous_hour,previous_secound_hour,total_load) : adds extra feature "previous_secound_hour" "**COMPLETED**"
 
 ## TODO
 
-#### HourBased:"Total Hour load"
-It could be usefull "consedering the time-frame for the project" to predict based on a total hour consumtion instead of using a minute to minute based prediction model.
-- hour_basic : (weekday,hour,total_load)   "**COMPLETED**"
-- hour_weekend : (weekday,hour,weekend_marker,total_load) : adds extra feature "weekend_marker" is a value that state if its a weekday or weekend  "**COMPLETED**"
-
-- hour_basic_prev_hour : (weekday,hour,previous_hour,total_load) : adds extra feature "previous_hour"
-- hour_basic_prev_hour2 : (weekday,hour,previous_hour,previous_secound_hour,total_load) : adds extra feature "previous_secound_hour"
 
 #### Sliding Window
 Because of the high non-linearizability nature of electric consumption .. 
---
+++ data_optimize/hour_basic_prev_hour.py
@@ -0,0 +1,124 @@
+from datetime import datetime
+
+
+##################################
+##          DATASET 	        ##
+##################################
+'''
+This is how the dataset look like looks like:
+---------------------------------------------
+[0]Date;[1]Time;[2]Global_active_power(kW);[3]Global_reactive_power;[4]Voltage;[5]Global_intensity;[6]Sub_metering_1;[7]Sub_metering_2;[8]Sub_metering_3
+Where:
+[6] sub_metering_1: energy sub-metering No. 1 (in watt-hour of active energy). It corresponds to the kitchen, containing mainly a dishwasher, an oven and a microwave (hot plates are not electric but gas powered). 
+[7] sub_metering_2: energy sub-metering No. 2 (in watt-hour of active energy). It corresponds to the laundry room, containing a washing-machine, a tumble-drier, a refrigerator and a light. 
+[8] sub_metering_3: energy sub-metering No. 3 (in watt-hour of active energy). It corresponds to an electric water-heater and an air-conditioner.
+'''
+
+##################################
+##          VARIABLES	        ##
+##################################
+
+hour_based=1  # (hour_based=1 : Total_load is the total load in an hour (scound_column range = 1->24), hour_based=0: Total_load is load in one minute (scound_column range = 1->1440))
+
+#input and output files
+output_file_name='_hour_basic_prev_hour'
+input_file='winter2006-2007.txt'
+
+# define costum accept condition for a record (for example : only mondays and hour=12) this is done in the script part but this is just a reminder
+accept_condition=1 
+
+##################################
+##          CONSTANTS	        ##
+##################################
+load_date=0
+load_timestamp=1
+
+#totalLoad
+active_power=2
+
+#interavtive load
+kitchen_load=6
+
+#back load
+laundryroom_load=7
+thermo_load=8
+
+#weekend
+weekend=[5,6]
+weekday=[0,1,2,3,4]
+
+
+# Vlaues for ploting
+value=[]  
+time_s=[]
+
+active_count=0
+total_count=0
+error_count=0
+
+hour_load=0
+current_hour=999
+day_of_week_update=0
+
+##################################
+##         	  SCRIPT	        ##
+##################################
+file_output = open(output_file_name,'w')  # The new out put file
+print " [*] New file created as '"+output_file_name+"' .."
+
+print " [*] Start reading file.."
+with open(input_file) as f:
+  for line in f:
+    # Deal with data row by row
+    row=line.split(';')
+    total_count+=1   
+
+    date_object = datetime.strptime(row[load_date], '%d/%m/%Y')
+    day_of_week= date_object.weekday()  
+   
+    (hour, m, s) = row[load_timestamp].split(':')
+    minute=(int(hour) * 3600 + int(m) * 60 + int(s))/60 # convert time to minutes
+    
+    try:
+      sum=float(row[kitchen_load])+float(row[laundryroom_load])+float(row[thermo_load])   # the sum of values (total load)
+    except ValueError:
+      error_count+=1
+      print " [*] ValueError ( "+str(error_count)+" ) at record , Date : '"+row[load_date]+"' , Time : '"+row[load_timestamp]+"' .."
+      continue  
+
+	  
+    if  accept_condition: #condition of feautre selection
+    
+      if hour_based:	  
+	    #Make a total consumtion for an hour
+        if current_hour==999:
+          current_hour=int(hour)
+
+        if current_hour==int(hour):
+          hour_load+=sum
+          day_of_week_update=day_of_week # the prevous hour might be in the prevous day
+          continue
+        else:
+          #Here it is a new hour
+          active_count+=1
+        
+          #new rule <------------------
+          value.append(hour_load)
+		  
+          if active_count>1:
+            file_output.write(str(day_of_week_update)+ ','+ str(current_hour+1)+ ','+ str(value[active_count-2])+ ','+str(hour_load)+'\n') # The final ourput (add +1 the current hour to avoid zero value)
+          current_hour=int(hour)
+          hour_load=sum       
+      
+      else:  #minute based
+        active_count+=1
+        file_output.write(str(day_of_week)+ ','+ str(minute)+ ','+str(sum)+'\n') # The final output the will be written to the file :: python will convert \n to os.linesep
+
+
+file_output.close() # you can omit in most cases as the destructor will call it  
+
+print ' [*] Done! '
+print '----------------------------'
+print 'Records Added: '+str(active_count)
+print 'Total Records: '+str(total_count)
+print 'Error Count  : '+str(error_count)
