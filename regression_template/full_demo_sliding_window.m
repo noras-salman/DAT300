2f16f4996531660b6f36a2f97ab97584415a9d11
noras-salman <noras.salman@live.com>
2016-10-16 14:10:50 +0200

    demo file fore easy execution

--- regression_template/full_demo_sliding_window.m
+++ regression_template/full_demo_sliding_window.m
@@ -15,61 +15,9 @@ dataset_name="D1";
 day_to_isolate=saturday;
  
 % ====== LOAD THE DATA FROM FILE 
- data = load('_hour_basic');
+file_name='_hour_basic';
  window_count=6;
  %+++++++++++++++++++++++++++++++++
  
- % ====== ISOLATE day
-day_of_week=data(:,1);
-isolated_dataset=data((day_of_week==day_to_isolate),:);
-isolated_dataset_size=size(isolated_dataset, 2);
-
-% ====== DAY OF THE WEEK IN A USELESS FEATURE.. IGNORE IT
-training_set=isolated_dataset(:,2:isolated_dataset_size);
-training_set_size=size(training_set, 2);
-features_cout=training_set_size-1;
-
-hour=training_set(:,1);
-  window_size=24/window_count;
-  
-  
-   full_hours=[];
- full_acctual=[];
- full_forecast=[];
- 
- 
- for w=1:window_count
- 
- lower_hour=w*window_size-window_size;
-
-
- higer_hour=w*window_size;
- 
-  window_set=training_set((hour>lower_hour & hour<=higer_hour),:);
- 
- [hours,acctual_load,forecasted_load]=single_window_forecast(window_set,features_cout,lower_hour+1:higer_hour);
- full_hours=[full_hours;hours];
- full_acctual=[full_acctual;acctual_load];
- full_forecast=[full_forecast;forecasted_load];
- 
- end;
-
- % Plot the original usage
-% =====================PLOT AND ANALYISIS===============
-
-mape = MAPE(full_acctual,full_forecast)
-
-
-plot(full_hours,full_acctual,'b','LineWidth',2)
-hold on
-
-%plot(predict(sample_range),plot_option,'LineWidth',2)
-plot(full_hours,full_forecast,'r','LineWidth',2)
-
-title(strcat('Sliding window STLF using dataset ',dataset_name))
-xlabel('Time of day (Hours)');
-ylabel('Electric Load (Watts)');
-legend('Real Data','Forecast');
-axis([1 24 0 inf]);
-figurename=strcat(dataset_name,"_sliding_window_forecast.png");
-saveas (1, figurename);
+%file_sliding_window(file_name,window_count,day_to_isolate,dataset_name)
+file_sliding_window(file_name,window_count,day_to_isolate,dataset_name)
