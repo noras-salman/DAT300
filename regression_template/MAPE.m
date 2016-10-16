c00806df48b2cf19655b1ab93c26958ec4fa13ee
noras-salman <noras.salman@live.com>
2016-10-16 11:10:45 +0200

    MAPE comment added

--- regression_template/MAPE.m
+++ regression_template/MAPE.m
@@ -1,5 +1,7 @@
 function mape_value = MAPE(actual_load,forecasted_load)
-
+ % Mean Absolute Percentage Error (MAPE)
+ % Computes the performance of the forecasting model
+ 
 APE=mean(abs(actual_load-forecasted_load)/actual_load);
 mape_value=sum(APE)*100;
 
